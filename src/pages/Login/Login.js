ce64c147edf3e7a2cbcae2dad0702249bb997513
Leechengwon <leecwee8497@gmail.com>
2023-09-01 17:26:40 +0900

    [49기 이청원]Weread 이관완료

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Login.scss';
+import React from "react";
+import "./Login.scss";
 
 const Login = () => {
-    return (
-        <div className="login">
-            <section className="flexCenter">이곳은 로그인 화면입니다.</section>
-            <Link to="/">메인 화면으로 이동하기</Link>
+  return (
+   
+    <div className="login">
+      <div className="imgBox">
+       <div>
+         <div><img className="imgLogoFrist" src="/wecodeLogo.png" /></div>
+         <div><img className="imgLogoTwo" src="/wecodeLogo2.png" /></div>
         </div>
-    );
-};
+      </div>
+      
+       <div className="loginBox">
+         
+          <input type="text" placeholder="이메일" className="email"></input>
+          <input type="text" placeholder="비밀번호" className="pass"></input>
+          <input type="button" className="buttonlogin"value="로그인"></input>
+         
+         <div className="smallBox">
+          <div>
+           <input type="button" className="join"value="회원가입"></input>
+          </div>
+          <div>
+           <input type="button" className="passFind"value="비밀번호 찾기"></input>
+          </div>
+          
+         </div>
 
+        </div>
+    </div>
+
+  
+  );
+};
+//<input class="searchInput" type="text" placeholder="과제 구현 검색창입니다.">
 export default Login;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Main.scss';
+import React from "react";
+import "./Main.scss";
 
 const Main = () => {
-    return (
-        <div className="main">
-            <section className="flexCenter">이곳은 메인 화면입니다.</section>
-            <Link to="/login">로그인 화면으로 이동하기</Link>
-        </div>
-    );
+  return (
+    <div className="allBox">
+      <div className="backArrow">
+        <div>뒤로</div>
+      </div>
+
+      <div>
+        <span>기본정보</span>
+        <span>필수사항</span>
+      </div>
+
+      <div>
+        <input type="text" placeholder="이메일" className="email"></input>
+      </div>
+      <div>
+        <input type="text" placeholder="비밀번호" className="pass"></input>
+      </div>
+      <div>
+        <input type="text" placeholder="비밀번호 g" className="pass"></input>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
